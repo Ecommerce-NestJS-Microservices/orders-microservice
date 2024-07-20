061f0b242e6c1be5d796aca4e12aef42e97836cc
mozadev <ceosmore@gmail.com>
2024-07-20 12:56:10 -0500

    Config orders Microservice

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Orders-Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port
+    }
+  });
   // await app.listen(3002);
-  await app.listen(envs.port)
-  logger .log(`Microservices running on port ${envs.port}`)
+  await app.listen()
+  logger.log(`Microservices running on port ${envs.port}`)
 
 }
 bootstrap();
